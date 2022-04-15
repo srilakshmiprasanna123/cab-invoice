5f4aec6d4e4c39d679484dd847dfb3b465693774
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-15 14:46:56 +0530

    multiple rides are added

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -4,11 +4,19 @@ public class CabInvoiceGenerator {
     public int MIN_FARE = 5;
     double totalFare;
 
-    public double CalculateFare(double DISTANCE, int TIME) {
+    public double calculateFare(double distance, int time) {
 
-        totalFare = COST_PER_KM * DISTANCE + COST_PER_MINUTE * TIME;
-        if (totalFare < MIN_FARE)
+        totalFare = COST_PER_KM * distance + COST_PER_MINUTE * time;
+        if(totalFare < MIN_FARE)
             totalFare = MIN_FARE;
+        return totalFare;
+    }
+
+    public double calculateFare(Rides[] rides) {
+        double totalFare = 0.0;
+        for (Rides ride : rides)
+            totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
+
         return totalFare;
     }
 }
